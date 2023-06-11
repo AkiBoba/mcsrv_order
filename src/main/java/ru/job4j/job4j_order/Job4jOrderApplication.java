07640d2edaafb0a08907a4b07186b77ce05d2cc5
Vladimir Likhachev <lettervl@ya.ru>
2023-06-11 03:51:28 +0300

    3. DockerFiles & Docker-Compose

--- src/main/java/ru/job4j/job4j_order/Job4jOrderApplication.java
+++ src/main/java/ru/job4j/job4j_order/Job4jOrderApplication.java
@@ -7,12 +7,45 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 import static java.lang.System.out;
 
 @SpringBootApplication
 @Configuration
 @EnableKafka
 public class Job4jOrderApplication {
+    private static String loadSysEnvIfNullThenConfig(String sysEnv, String key, Properties config) {
+    String value = System.getenv(sysEnv);
+    if (value == null) {
+        value = config.getProperty(key);
+    }
+    return value;
+}
+
+    private static Connection loadConnection() throws ClassNotFoundException, SQLException {
+        var config = new Properties();
+        try (InputStream in = Job4jOrderApplication.class.getClassLoader()
+                .getResourceAsStream("application.properties")) {
+            config.load(in);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+        String url = loadSysEnvIfNullThenConfig("JDBC_URL", "url", config);
+        String username = loadSysEnvIfNullThenConfig("JDBC_USERNAME", "username", config);
+        String password = loadSysEnvIfNullThenConfig("JDBC_PASSWORD", "password", config);
+        String driver = loadSysEnvIfNullThenConfig("JDBC_DRIVER", "driver-class-name", config);
+        System.out.println("url=" + url);
+        Class.forName(driver);
+        return DriverManager.getConnection(
+                url, username, password
+        );
+    }
 
     @Bean
     public RestTemplate getTemplate() {
